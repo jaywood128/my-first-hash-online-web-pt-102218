ba739f70cfbd943fe84cbf81af8abe1475d58d51
Johnathon Wood <jaywood128@gmail.com>
2018-10-29 19:10:09 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,7 +3,7 @@ def my_hash
 
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.end
 
-
+end
 def shipping_manifest
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" =>3}
   # set a variable called `the_manifest`, equal to a hash
