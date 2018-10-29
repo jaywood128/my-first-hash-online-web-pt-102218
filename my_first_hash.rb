d0c420d4b391f2bbb28bb5271f7910e7762ab3f1
Johnathon Wood <jaywood128@gmail.com>
2018-10-29 19:09:06 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,8 +9,6 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-
-
 end
 
 def retrieval
@@ -40,4 +38,5 @@ def adding
 
   # return the shipping_manifest hash below
 
+end
 end
